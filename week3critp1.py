a76c3b25e1814fee05cdc600a5c74907810c7e4c
kcaldarone <99760587+kcaldarone@users.noreply.github.com>
2024-08-23 13:39:08 -0400

    Added more code

--- week3critp1.py
+++ week3critp1.py
@@ -1,21 +1,22 @@
 '''
 Name: Kori Caldarone
-Assignment: Week 3 Critical Thinking Part 1
+Assignment: Week 1 Critical Thinking
 Professor: Professor Schwartz
 Date: 8/23/2024
 '''
-#User inputs food price
+
+#User inputs meal price
 total = float(input("And how much does the customer owe for the meal?\n"))
-print("Alright, the customer owes $"+ str(total) + " for their meal tonight.")
+print("Alright, the customer owes $"+ str(round(total,2)) + " for their meal tonight.")
 
 #calculating 7% tax 
-taxedTotal = total * 1.07
-taxAmt = taxedTotal - total
+taxedTotal = round((total * 1.07), 2)
+taxAmt = round((taxedTotal - total), 2)
 
 #calculating 18% tip with taxed total as tip comes after tax is added
-tippedTotal = taxedTotal * 1.18
-tipAmt = tippedTotal - taxedTotal
+tippedTotal = round((taxedTotal * 1.18), 2)
+tipAmt = round((tippedTotal - taxedTotal), 2)
 
 print("With a sales tax rate of 7%, the amount plus tax would be taxed $" + str(taxAmt) + ", which would bring the total to $" + str(taxedTotal) + ".\n")
-print("In addition to this, if you wish to leave an 18% tip for your waitress, that would be an additional $" + str(tipAmt) + ", bringing the customers total to $" + str(tippedTotal))
+print("In addition to this, if you wish to leave an 18% tip for your waitress, that would be an additional $" + str(tipAmt) + ", bringing the customer's total to $" + str(tippedTotal) +".")
 
