b2d65bbccb8bce74d0a7146f56244bbc391d1769
kcaldarone <99760587+kcaldarone@users.noreply.github.com>
2024-09-21 15:01:09 -0400

    Week 6 Discussion Code

---
+++ Discussion Posts/week6discussionpost.py
@@ -0,0 +1,24 @@
+#Week 6 Discussion Post - Lists and Dictionaries
+
+#Lists - Insert
+names = []
+names.insert(0,"Caleb")
+
+#Lists - Insert (Alternate Method (Append))
+names.append("Melissa")
+
+#Lists - Insert (Alternate Method (Extend))
+moreNames = ["Kayla", "James", "Corey"]
+names.extend(moreNames)
+
+#Lists - Update
+names[0] = "Rebecca"
+
+#Lists - Remove
+print(names)
+
+#Dictionaries - Insert  
+
+#Dictionaries - Update
+
+#Dictionaries - Remove
