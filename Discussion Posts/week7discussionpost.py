ff29da04912868eeacf425f60f60c44e06839ec4
kcaldarone <99760587+kcaldarone@users.noreply.github.com>
2024-09-25 18:09:15 -0400

    Create week7discussionpost.py

---
+++ Discussion Posts/week7discussionpost.py
@@ -0,0 +1,16 @@
+#User-Implemented Function to find the length of an array
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+size = 0
+for i in arr:
+    size = size + 1
+#print("The size of the array is " + str(size))
+
+#Predefined Function to find the length of an array
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+print("The size of the array is " + str(len(arr)))
+
+#Complex User-Defined Method/Function
+#See portfolio
+
